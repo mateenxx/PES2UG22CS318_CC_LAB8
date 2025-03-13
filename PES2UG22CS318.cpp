f152d0516d8a6dff16d5f6afc0c186acaaddd252
mateenxx <154893909+mateenxx@users.noreply.github.com>
2025-03-13 10:50:47 +0530

    Create PES2UG22CS318.cpp

---
+++ PES2UG22CS318.cpp
@@ -0,0 +1,6 @@
+#include <iostream>  // Include input-output stream library
+
+int main() {
+    std::cout << "PES2UG22CS318!" << std::endl;  // Print message to console
+    return 0;  // Indicate successful execution
+}
